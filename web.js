3639ddfb6a8c51eac131e1fe8b31d4b648c78ad6
Alexandru Strajeriu <giust00@rockectmail.com>
2013-07-04 06:35:23 +0000

    test 2 of hello world from index

--- web.js
+++ web.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var fs      = require('fs');
-var buffer  = require('buf');
 
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync(index.html);
-var uncode = text.toString();
 app.get('/', function(request, response) {
-  response.send(uncode);
+  var buffer = fs.readFileSync(index.html);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
